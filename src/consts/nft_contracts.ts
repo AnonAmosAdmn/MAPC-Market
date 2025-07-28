13510d9c0257f464d32d513ca416e200e87688b1
AnonAmosAdmn <122024790+AnonAmosAdmn@users.noreply.github.com>
2025-07-28 18:31:32 -0500

    Update nft_contracts.ts

--- src/consts/nft_contracts.ts
+++ src/consts/nft_contracts.ts
@@ -23,14 +23,6 @@ export const NFT_CONTRACTS: NftContract[] = [
     type: "ERC721",
   },
   {
-    address: "0xDBb7b5AB71c162e41EEC9c9D7a6F9326530fCeD0",
-    chain: monadTestnet,
-    title: "MetaFoxes",
-    thumbnailUrl:
-      "/mf.jpg",
-    type: "ERC1155",
-  },
-  {
     address: "0xdD7E7c7d8525F8C749BDD592656e9f970100d5Bd",
     chain: monadTestnet,
     title: "Monad Toads",
